719a9c6d8e1a87be84e5a512558d950467ed8bd7
hoanglongraspi <longh1686@gmail.com>
2024-11-24 01:41:36 -0500

    project

--- Project101/settings.py
+++ Project101/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'  # Example: 'login' if your login URL is '/login/'
